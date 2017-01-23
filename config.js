21f2761fd54151d10fd7c7a5432a098ae001fed3
amitrsingh1992 <singh.amit.10ee1074@gmail.com>
2017-01-23 19:13:16 +0530

    database

---
+++ config.js
@@ -0,0 +1,65 @@
+var config = {
+   apiKey: "AIzaSyBPzumhlybSGUi6hHOb2421ZAPJM-487Lo",
+   authDomain: "fundoohr-d4100.firebaseapp.com",
+   databaseURL: "https://fundoohr-d4100.firebaseio.com",
+   storageBucket: "fundoohr-d4100.appspot.com",
+   messagingSenderId: "52019277874"
+ };
+ firebase.initializeApp(config);
+ // var dummy= {
+ //      "bank" : {
+ //        "accountNumber" : "06001251551000",
+ //        "bankName" : "SBI",
+ //        "ifscCode" : "SBI002121",
+ //        "pan" : "APZT54551ds",
+ //        "paySalary" : "Yes",
+ //        "reason" : "NA"
+ //      },
+ //      "educational" : {
+ //        "aggregateIn" : "71",
+ //        "degree" : "Btech",
+ //        "diploma" : "yes",
+ //        "discipline" : "CS",
+ //        "finalYearPercentage" : "76",
+ //        "training" : "NA",
+ //        "trainingInstitute" : "NA",
+ //        "trainingPeriod" : "6 months",
+ //        "yearOfPassing" : "2016"
+ //      },
+ //      "hrData" : {
+ //        "blStartDate" : "1477308028",
+ //        "compContractInitiated" : "Yes",
+ //        "compContractSigned" : "No",
+ //        "company" : "BridgeLabz",
+ //        "companyJoinDate" : "1477308028",
+ //        "companyLeaveDate" : "1477308028",
+ //        "contractSignDate" : "..",
+ //        "employeeStatus" : "Fellowship",
+ //        "enggContractInitiated" : "Yes",
+ //        "enggContractSigned" : "Yes",
+ //        "fellowshipPeriod" : "3 Months",
+ //        "hiringCity" : "Mumbai",
+ //        "initiateTransfer" : "Yes"
+ //      },
+ //      "personal" : {
+ //        "annualSalary" : "20L",
+ //        "dateOfBirth" : "20 Sep 1993",
+ //        "emailId" : "ganguly01@gmail.com",
+ //        "employeeName" : "Abhishek G",
+ //        "fatherMobile" : "9768525131",
+ //        "fatherName" : "KK ganguli",
+ //        "mobile" : "9876000021",
+ //        "mumbaiAddress" : "Flat No.201,Raheja Complex , Andheri (W)",
+ //        "occupation" : "Engineer",
+ //        "permenantAddress" : "Flat No.201, Raheja Complex , Andheri (W)"
+ //      },
+ //      "tracking" : {
+ //        "bridgelabzEndDate" : "2nd April 2017",
+ //        "bridgelabzStartDate" : "2nd Jan 2017",
+ //        "currentWeek" : "4 week",
+ //        "numberOfWeeksLeft" : "19 week",
+ //        "numberWeeksLeft" : "7 weeks",
+ //        "techStack" : "Android",
+ //        "week1" : "7 week"
+ //      }
+ //    }
